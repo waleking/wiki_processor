f884ae104031066296e0d724a7198a1848e83f1f
huangwaleking <huangwaleking@gmail.com>
2016-06-01 01:32:19 +0800

    ready to run hits

--- additional_scripts/compute_hits.py
+++ additional_scripts/compute_hits.py
@@ -1,14 +1,21 @@
 #encoding=utf-8
-import networkx
+import networkx as nx
 
-def load(lines):
+def load(filename):
     '''treat cat,subCat as target, source
         treat cat, page as target, source in catToPageGraph.txt
     '''
     g=nx.DiGraph()
-    for line in lines:
-        source,target=line.strip().split(" ")
+    for line in open(filename,"r"):
+        source,target=line.strip().split("\t")
         g.add_edges_from([("cat_"+source,"page_"+target)])
-        return g
+    return g
 
-load("catToPageGraph.txt")
+g=load("catToPageGraph.txt")
+authoriescores,hubscores=nx.hits(g)
+fWriter=open("hits_score.txt","w")
+for page in hubscores:
+    score=hubscores[page]
+    page=page.split("cat_")[-1]
+    fWriter.write("%s\t%s\n" % (page,score))
+fWriter.close()
